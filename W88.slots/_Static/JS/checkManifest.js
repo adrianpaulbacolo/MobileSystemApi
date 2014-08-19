740651c367c9ff111713181bc2e173d2b1451936
VisualSVN Server <VisualSVN Server@23a21216-149f-b64c-b2d1-54b469129133>
2014-08-19 10:27:28 +0000

    Deleted folder '/trunk/W88.slots/_Static/JS'.
    git-svn-id: https://192.168.1.192/svn/MobileWeb/trunk@15 23a21216-149f-b64c-b2d1-54b469129133

--- W88.slots/_Static/JS/checkManifest.js
+++
@@ -1,1 +0,0 @@
-﻿window.applicationCache.addEventListener("updateready", function (a) { if (window.applicationCache.status == window.applicationCache.UPDATEREADY) { window.applicationCache.swapCache(); if (confirm("A new version of this site is available. Load it?")) { window.location.reload() } } }, false)
