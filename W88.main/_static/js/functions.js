1bc0bda23412c0d578f852ad91e31ea41f03b944
VisualSVN Server <VisualSVN Server@23a21216-149f-b64c-b2d1-54b469129133>
2015-01-26 01:45:13 +0000

    Deleted folder '/trunk/W88.main'.
    git-svn-id: https://192.168.1.192/svn/MobileWeb/trunk@129 23a21216-149f-b64c-b2d1-54b469129133

--- W88.main/_static/js/functions.js
+++
@@ -1,9 +0,0 @@
-﻿jQuery.getParams = function (s) {
-    var url = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-    for (var i = 0; i < url.length; i++) {
-        var urlparam = url[i].split('=');
-        if (urlparam[0] == param) {
-            return urlparam[1];
-        }
-    }
-}
