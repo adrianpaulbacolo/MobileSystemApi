28eed424701c84beed50d8a5d536c7ef9aa054cd
jaysonE <jayson.eduardo@spiralwks.com>
2015-08-13 14:17:52 +0800

    alternative fix for SDPay enable CORS

--- W88.m/_Static/JS/Main.js
+++ W88.m/_Static/JS/Main.js
@@ -4,6 +4,7 @@
         $.ajax({
             contentType: "application/json; charset=utf-8",
             url: "/_secure/AjaxHandlers/MemberSessionCheck.ashx",
+            crossDomain: true,
             /*dataType: "jsonp",*/
             success: function (data) {
                 if (data != '1' && data != '-1') { window.location.replace('/Expire'); }
