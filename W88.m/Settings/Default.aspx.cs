ff61906204ddf885cf30492046866ac5cf6e9fb6
Mussulini Lopez <darkmuzo@yahoo.com>
2015-12-03 20:54:30 +0800

    Profile, Settings and fix navigations

---
+++ W88.m/Settings/Default.aspx.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Settings_Default : System.Web.UI.Page {
+
+}
