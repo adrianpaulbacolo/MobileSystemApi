0d722f86bd7989caf6a73e554cd22fadb8c85d50
Ruby Sarah Sitchon <ruby.sitchon@mnltechnology.com>
2016-03-08 15:29:58 +0800

    Add radar cedexis script

---
+++ W88.m/_Static/JS/radar.js
@@ -0,0 +1,15 @@
+﻿(function (e, c, a, g, f) {
+    function d() {
+        var b = c.createElement("script");
+        b.async = !0;
+        b.src = "//radar.cedexis.com/1/14375/radar.js";
+        c.body.appendChild(b)
+    }
+    (function () {
+        for (var b = [/\bMSIE (5|6)/i], a = b.length; a--;)
+            if (b[a].test(navigator.userAgent))
+                return !1;
+        return !0
+    })()
+    && ("complete" !== c.readyState ? (a = e[a]) ? a(f, d, !1) : (a = e[g]) && a("on" + f, d) : d())
+})(window, document, "addEventListener", "attachEvent", "load");
