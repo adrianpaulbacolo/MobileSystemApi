0754d04177f7c5af01ecd58c004bf620be9561d9
Mark <mark.marquez@mnltechnology.com>
2016-04-29 12:07:23 +0800

    User: Refactor seamless login, add session check
    - Move decrypt to commonEncryption
    - Reuse user session check in seamless login
    - Add redirection to login if token is not valid

--- W88.m/_Secure/Login.aspx.cs
+++ W88.m/_Secure/Login.aspx.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
 using System.Linq;
-using System.Security.Cryptography;
 using System.Security.Policy;
 using System.Text;
 using System.Web;
@@ -52,75 +51,30 @@ public partial class _Secure_Login : BasePage
 
         if (!string.IsNullOrEmpty(Request.QueryString.Get("token")))
         {
-            
             try
             {
                 var cipherKey = commonEncryption.Decrypt(ConfigurationManager.AppSettings.Get("PrivateKeyToken"));
-                strSessionId = decryptToken(Request.QueryString.Get("token"), cipherKey);
-                
+                strSessionId = commonEncryption.decryptToken(Request.QueryString.Get("token"), cipherKey);
+                commonVariables.SetSessionVariable("CurrentMemberSessionId", strSessionId);
 
-                using (wsMemberMS1.memberWSSoapClient svcInstance = new wsMemberMS1.memberWSSoapClient())
+                var loginCode = UserSession.checkSession();
+
+                if (loginCode != "1")
                 {
-                    System.Data.DataSet dsSignin = null;
-                    dsSignin = svcInstance.MemberSessionCheck(strSessionId, commonIp.UserIP);
-
-                    if (dsSignin.Tables[0].Rows.Count > 0)
-                    {
-                        strProcessCode = Convert.ToString(dsSignin.Tables[0].Rows[0]["RETURN_VALUE"]);
-                        switch (strProcessCode)
-                        {
-                            case "0":
-                                strProcessMessage = commonCulture.ElementValues.getResourceString("Exception", xeErrors);
-                                break;
-                            case "1":
-                                string strMemberSessionId = Convert.ToString(dsSignin.Tables[0].Rows[0]["memberSessionId"]);
-                                HttpContext.Current.Session.Add("MemberSessionId", Convert.ToString(dsSignin.Tables[0].Rows[0]["memberSessionId"]));
-                                HttpContext.Current.Session.Add("MemberId", Convert.ToString(dsSignin.Tables[0].Rows[0]["memberId"]));
-                                HttpContext.Current.Session.Add("MemberCode", Convert.ToString(dsSignin.Tables[0].Rows[0]["memberCode"]));
-                                HttpContext.Current.Session.Add("CountryCode", Convert.ToString(dsSignin.Tables[0].Rows[0]["countryCode"]));
-                                HttpContext.Current.Session.Add("CurrencyCode", Convert.ToString(dsSignin.Tables[0].Rows[0]["currencyCode"]));
-                                HttpContext.Current.Session.Add("LanguageCode", Convert.ToString(dsSignin.Tables[0].Rows[0]["languageCode"]));
-                                HttpContext.Current.Session.Add("RiskId", Convert.ToString(dsSignin.Tables[0].Rows[0]["riskId"]));
-                                //HttpContext.Current.Session.Add("PaymentGroup", "A"); //Convert.ToString(dsSignin.Tables[0].Rows[0]["paymentGroup"]));
-                                HttpContext.Current.Session.Add("PartialSignup", Convert.ToString(dsSignin.Tables[0].Rows[0]["partialSignup"]));
-                                HttpContext.Current.Session.Add("ResetPassword", Convert.ToString(dsSignin.Tables[0].Rows[0]["resetPassword"]));
-
-
-                                commonCookie.CookieS = strMemberSessionId;
-                                commonCookie.CookieG = strMemberSessionId;
-                                HttpContext.Current.Session.Add("LoginStatus", "success");
-                                break;
-                            case "10":
-                                strProcessMessage = "NotLogin";
-                                commonVariables.ClearSessionVariables();
-                                commonCookie.ClearCookies();
-                                break;
-                            case "13":
-                                commonVariables.ClearSessionVariables();
-                                commonCookie.ClearCookies();
-                                break;
-                            case "21":
-                                strProcessMessage = commonCulture.ElementValues.getResourceXPathString("Login/InvalidUsername", xeErrors);
-                                break;
-                            case "22":
-                                strProcessMessage = commonCulture.ElementValues.getResourceXPathString("Login/InactiveAccount", xeErrors);
-                                break;
-                            case "23":
-                                strProcessMessage = commonCulture.ElementValues.getResourceXPathString("Login/InvalidPassword", xeErrors);
-                                break;
-                        }
-                    }
+                    commonVariables.ClearSessionVariables();
+                    commonCookie.ClearCookies();
+                    Response.Redirect("/_Secure/Login_app.aspx");
+                }
+                else
+                {
+                    Response.Redirect("/Deposit/Default_app.aspx");
                 }
             }
             catch (Exception ex)
             {
-                strProcessCode = "0";
-                strProcessMessage = ex.Message;
-                commonVariables.ClearSessionVariables();
-                commonCookie.ClearCookies();
+                Response.Redirect("/Deposit/Default_app.aspx");
             }
 
-            Response.Redirect("/Deposit/Default_app.aspx");
         }
         else
         {
@@ -151,36 +105,5 @@ public partial class _Secure_Login : BasePage
 
             lblRegister.Text = commonCulture.ElementValues.getResourceString("btnRegister", xeResources);
         }
-    }
-
-    public static string decryptToken(string cipherString,string cipherKey)
-    {
-        byte[] keyArray;
-        //get the byte code of the string
-
-        byte[] toEncryptArray = Convert.FromBase64String(cipherString);
-
-        System.Configuration.AppSettingsReader settingsReader = new AppSettingsReader();
-
-        //if hashing was not implemented get the byte code of the key
-        keyArray = UTF8Encoding.UTF8.GetBytes(cipherKey);
-
-        TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
-        //set the secret key for the tripleDES algorithm
-        tdes.Key = keyArray;
-        //mode of operation. there are other 4 modes.
-        //We choose ECB(Electronic code Book)
-
-        tdes.Mode = CipherMode.ECB;
-        //padding mode(if any extra byte added)
-        tdes.Padding = PaddingMode.PKCS7;
-
-        ICryptoTransform cTransform = tdes.CreateDecryptor();
-        byte[] resultArray = cTransform.TransformFinalBlock
-                (toEncryptArray, 0, toEncryptArray.Length);
-        //Release resources held by TripleDes Encryptor
-        tdes.Clear();
-        //return the Clear decrypted TEXT
-        return UTF8Encoding.UTF8.GetString(resultArray);
     }
 }
