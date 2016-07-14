4f73df041df0da757d214813ecf812e3e550447d
Jack Ohdo <jack.ohdo@mnltechnology.com>
2016-07-14 17:56:53 +0800

    Move code inside

--- W88.m/_Static/JS/modules/fundtransfer.js
+++ W88.m/_Static/JS/modules/fundtransfer.js
@@ -6,32 +6,33 @@
         }
     };
 
+    function GetWallets() {
+
+        var loader = GPInt.prototype.GetLoaderScafold();
+
+        $.ajax({
+            type: 'POST',
+            contentType: "application/json",
+            url: '/AjaxHandlers/GetWalletsWithBalance.ashx',
+            beforeSend: function () {
+                $('span[name="WalletBalance"]').html(loader);
+            },
+            success: function (xml) {
+                if (xml != "-1") {
+                    $.each(xml, function (i, val) {
+                        var id = val.Key;
+                        $("#" + id).html(val.Value);
+                    });
+                } else {
+                    window.location.reload();
+                }
+            },
+            error: function (err) {
+            }
+        });
+    }
+
     return Wallets;
-}
-
-var loader = GPInt.prototype.GetLoaderScafold();
-
-function GetWallets() {
-    $.ajax({
-        type: 'POST',
-        contentType: "application/json",
-        url: '/AjaxHandlers/GetWalletsWithBalance.ashx',
-        beforeSend: function () {
-            $('span[name="WalletBalance"]').html(loader);
-        },
-        success: function (xml) {
-             if (xml != "-1") {
-                 $.each(xml, function (i, val) {
-                     var id = val.Key;
-                     $("#" + id).html(val.Value);
-                 });
-             } else {
-                 window.location.reload();
-             }
-        },
-        error: function (err) {
-        }
-    });
 }
 
 window.w88Mobile.Wallets = Wallets();
