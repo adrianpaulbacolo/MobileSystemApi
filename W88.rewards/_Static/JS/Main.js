517da57a57743329f76ac20910519f5af5f1ca8e
ajftolentino <josephtolentino@ymail.com>
2016-08-11 15:45:12 +0800

    Rewards Mobile: Update header files
    - Update header.inc and header.shtml
    - Add new css files.
    - Use latest jQuery mobile minified version (1.4.5) for css and js.
    - Add new image resources.
    - Update Main.js and add function for checking cookie.
    - Update GPINT.js to enable showing of new splash screen.
    - Add growl.js for showing of popup for messages.

--- W88.rewards/_Static/JS/Main.js
+++ W88.rewards/_Static/JS/Main.js
@@ -1,26 +1,36 @@
 ﻿$(window).load(function () {
     GPINTMOBILE.HideSplash();
-    window.setInterval(function () {
-        
-        $.ajax({
-            contentType: "application/json; charset=utf-8",
-            url: "/Rewards/_secure/AjaxHandlers/MemberSessionCheck.ashx",
-            /*dataType: "jsonp",*/
-            success: function (data) {
-                if (data != '1' && data != '-1') { window.location.replace('/Expire'); }
-            },
-            error: function (err) {
-                //console.log(err);
-            }
-        });
-    }, 5000);
+    if (typeof window.User != "undefined" && window.User.hasSession) checkSession();
+    var sessionPoll;
+
+    function checkSession() {
+        var intervalMin = 10000; // 10 secs
+        var sessionInterval = (typeof window.User != "undefined" && parseInt(window.User.sessionInterval) > intervalMin) ? parseInt(window.User.sessionInterval) : intervalMin;
+
+        sessionPoll = window.setInterval(function () {
+            $.ajax({
+                contentType: "application/json; charset=utf-8",
+                url: "/_secure/AjaxHandlers/MemberSessionCheck.ashx",
+                responseType: "json",
+                success: function (data) {
+                    if (data.code != "1") {
+                        if (typeof data.message != "undefined") alert(data.message);
+                        clearInterval(sessionPoll);
+                        window.location.replace("/Logout");
+                    }
+                },
+                error: function (err) {
+                }
+            });
+        }, sessionInterval);
+    }
+
+    $(".navbar-toggle").on("click", function () {
+        $(this).toggleClass("active");
+    });
 });
 
 if ($('#divBalance').hasClass('open')) { $('#divBalance').addClass('close'); } else { if ($('#divBalance').hasClass('open')) { $('#divBalance').addClass('close'); } }
-
-// mozfullscreenerror event handler
-function errorHandler() { /*alert('mozfullscreenerror');*/ }
-//document.documentElement.addEventListener('mozfullscreenerror', errorHandler, false);
 
 // toggle full screen
 function toggleFullScreen() {
@@ -44,11 +54,31 @@ function toggleFullScreen() {
     }
 }
 
-// keydown event handler
-/*
-document.addEventListener('keydown', function(e) {
-  if (e.keyCode == 13 || e.keyCode == 70) { // F or Enter key
-    toggleFullScreen();
-  }
-}, false);
-*/
+function Cookies() {
+    var setCookie = function (cname, cvalue, expiryDays) {
+        var date = new Date();
+        date.setTime(date.getTime() + (expiryDays * 24 * 60 * 60 * 1000));
+        var expires = "expires=" + date.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    };
+
+    var getCookie = function (cname) {
+        var name = cname + "=";
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i];
+            while (cookie.charAt(0) == ' ')
+                cookie = cookie.substring(1);
+
+            if (cookie.indexOf(name) == 0) {
+                return cookie.substring(name.length, cookie.length);
+            }
+        }
+        return "";
+    };
+
+    return {
+        setCookie: setCookie,
+        getCookie: getCookie
+    };
+}
