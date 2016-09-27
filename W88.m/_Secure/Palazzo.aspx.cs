04c6450c3c536da9236f24efe17b1581b839aff1
jackohdo <jack.ohdo@mnltechnology.com>
2016-09-27 10:42:29 +0800

    PT Login form

---
+++ W88.m/_Secure/Palazzo.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Secure_Palazzo : System.Web.UI.Page
+{
+    System.Xml.Linq.XElement _xeLoginResources;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        commonCulture.appData.getRootResource("/_Secure/Login.aspx", out _xeLoginResources);
+
+        lblRegister.Text = commonCulture.ElementValues.getResourceString("lblRegister2", _xeLoginResources);
+        lblRegNote.Text = commonCulture.ElementValues.getResourceString("lblMsgNote", _xeLoginResources);
+        lblUsername.Text = commonCulture.ElementValues.getResourceString("lblUsername", _xeLoginResources);
+        lblPassword.Text = commonCulture.ElementValues.getResourceString("lblPassword", _xeLoginResources);
+    }
+}
