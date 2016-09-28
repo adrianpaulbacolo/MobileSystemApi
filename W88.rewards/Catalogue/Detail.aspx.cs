f82511d9d9f0f468363f7d50d9e7d73ee2b830b6
ajftolentino <josephtolentino@ymail.com>
2016-09-28 11:32:45 +0800

    Move GetProductDetails method to RewardsHelper

--- W88.rewards/Catalogue/Detail.aspx.cs
+++ W88.rewards/Catalogue/Detail.aspx.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Data;
 using System.Web;
 using System.Text;
-using W88.BusinessLogic.Accounts.Models;
 using W88.BusinessLogic.Rewards.Helpers;
-using W88.BusinessLogic.Rewards.Models;
 using W88.Utilities;
 using W88.Utilities.Log.Helpers;
 
@@ -28,7 +25,7 @@ public partial class Catalogue_Detail : CatalogueBasePage
         try
         {
             var productId = HttpContext.Current.Request.QueryString.Get("id");
-            var productDetails = GetProductDetails(MemberSession, productId);
+            var productDetails = (new RewardsHelper()).GetProductDetails(MemberSession, productId, HasSession);
 
             if (productDetails == null)
             {
@@ -97,135 +94,6 @@ public partial class Catalogue_Detail : CatalogueBasePage
         {
             var memberCode = UserSessionInfo == null ? string.Empty : UserSessionInfo.MemberCode;
             AuditTrail.AppendLog(memberCode, "/Catalogue/Detail.aspx", "RedeemProduct", "Catalogue", string.Empty, string.Empty, string.Empty, exception.Message, (new Guid()).ToString(), string.Empty, string.Empty, true);
-        }
-    }
-
-    private ProductDetails GetProductDetails(MemberSession memberSession, string productId)
-    {
-        try
-        {
-            var detailsSet = RewardsHelper.GetProductDetails(MemberSession, productId);
-            if (detailsSet.Tables.Count == 0 || detailsSet.Tables[0].Rows.Count == 0)
-            {
-                return null;
-            }
-
-            var dataRow = detailsSet.Tables[0].Rows[0];
-            if (dataRow == null)
-            {
-                return null;
-            }
-            dataRow["pointsRequired"] = Convert.ToInt32(dataRow["pointsRequired"].ToString().Replace(" ", string.Empty));
-
-            if (!detailsSet.Tables[0].Columns.Contains("pointsLeveldiscount"))
-            {
-                detailsSet.Tables[0].Columns.Add("pointsLeveldiscount");
-                dataRow["pointsLeveldiscount"] = 0;
-            }
-
-            if (!detailsSet.Tables[0].Columns.Contains("pointsRequired2"))
-            {
-                detailsSet.Tables[0].Columns.Add("pointsRequired2");
-                dataRow["pointsRequired2"] = dataRow["pointsRequired"];
-            }
-
-            if (!detailsSet.Tables[0].Columns.Contains("discountPercentage"))
-            {
-                detailsSet.Tables[0].Columns.Add("discountPercentage");
-                dataRow["discountPercentage"] = 0;
-            }
-
-            if (dataRow["discountPoints"] == DBNull.Value)
-            {
-                if (HasSession && dataRow["productType"].ToString() != "1")
-                {
-                    //grab member point level
-                    var pointLevelDiscount = RewardsHelper.GetMemberPointLevelDiscount(MemberSession);
-                    var percentage = Convert.ToDouble(pointLevelDiscount) / 100;
-                    var normalPoint = int.Parse(dataRow["pointsRequired"].ToString());
-                    var points = Math.Floor(normalPoint * (1 - percentage));
-                    var pointAfterLevelDiscount = Convert.ToInt32(points);
-
-                    dataRow["pointsRequired"] = pointAfterLevelDiscount;
-                    dataRow["pointsLeveldiscount"] = pointAfterLevelDiscount;
-                    dataRow["discountPercentage"] = pointLevelDiscount;
-                }    
-            }
-
-            dataRow["imageName"] =
-                Convert.ToString(
-                    Common.GetAppSetting<string>("ImagesDirectoryPath") +
-                    "Product/" + dataRow["imageName"]);
-
-            var riskId = memberSession == null ? "" : memberSession.RiskId;
-            if (!string.IsNullOrEmpty(riskId))
-            {
-                //valid category
-                dataRow["redemptionValidityCat"] += ",";
-                if (dataRow["redemptionValidityCat"].ToString().ToUpper() != "ALL,")
-                {
-                    if (!((string)dataRow["redemptionValidityCat"]).Contains(riskId + ","))
-                    {
-                        dataRow["redemptionValidityCat"] = "0";
-                    }
-                    else
-                    {
-                        dataRow["redemptionValidityCat"] = "1";
-                    }
-                }
-                else
-                {
-                    dataRow["redemptionValidityCat"] = "1";
-                }
-
-                dataRow["redemptionValidity"] += ",";
-                if (dataRow["redemptionValidity"].ToString().ToUpper() != "ALL,")
-                {
-                    if (!((string)dataRow["redemptionValidity"]).Contains(riskId + ","))
-                    {
-                        dataRow["redemptionValidity"] = "0";
-                    }
-                    else
-                    {
-                        dataRow["redemptionValidity"] = "1";
-                    }
-                }
-                else
-                {
-                    dataRow["redemptionValidity"] = "1";
-                }
-            }
-            else
-            {
-                dataRow["redemptionValidity"] += "0";
-                dataRow["redemptionValidityCat"] += "0";
-            }
-
-            // Set product details
-            var productDetails = new ProductDetails();
-            productDetails.ProductId = productId;
-            productDetails.ProductType = dataRow["productType"].ToString();
-            productDetails.AmountLimit = dataRow["amountLimit"].ToString();
-            productDetails.CategoryId = dataRow["categoryId"].ToString();
-            productDetails.CurrencyCode = dataRow["currencyValidity"].ToString();
-            productDetails.CountryCode = dataRow["countryValidity"].ToString();
-            productDetails.PointsRequired = dataRow["pointsRequired"].ToString();
-            productDetails.ImageUrl = dataRow["imageName"].ToString();
-            productDetails.ProductCategoryName = dataRow["categoryName"].ToString();
-            productDetails.ProductName = dataRow["productName"].ToString();
-            productDetails.ProductDescription = dataRow["productDescription"].ToString();
-            productDetails.DeliveryPeriod = dataRow["deliveryPeriod"].ToString();
-            productDetails.DiscountPoints = dataRow["discountPoints"] == DBNull.Value ? string.Empty : dataRow["discountPoints"].ToString();
-            productDetails.RedemptionValidity = dataRow["redemptionValidity"].ToString();
-            productDetails.RedemptionValidityCategory = dataRow["redemptionValidityCat"].ToString();
-            
-            return productDetails;
-        }
-        catch (Exception exception)
-        {
-            var memberCode = UserSessionInfo == null ? string.Empty : UserSessionInfo.MemberCode;
-            AuditTrail.AppendLog(memberCode, "/Catalogue/Detail.aspx", "RedeemProduct", "Catalogue", string.Empty, string.Empty, string.Empty, exception.Message, (new Guid()).ToString(), string.Empty, string.Empty, true);
-            return null;
         }
     }
 
