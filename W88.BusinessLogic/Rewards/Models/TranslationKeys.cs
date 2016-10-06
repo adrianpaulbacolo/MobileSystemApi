a5340f7393bf2473923bfc60fdb5addbf21626da
ajftolentino <josephtolentino@ymail.com>
2016-10-06 12:12:33 +0800

    Add TranslationKeys class

---
+++ W88.BusinessLogic/Rewards/Models/TranslationKeys.cs
@@ -0,0 +1,46 @@
+﻿
+
+namespace W88.BusinessLogic.Rewards.Models
+{
+    public class TranslationKeys
+    {
+        public static class Redemption 
+        {
+            public const string MailSubject = "Redemption_MailSubject";
+            public const string MailBody = "Redemption_MailBody";
+            public const string InvalidQuantity = "Redemption_InvalidQuantity";
+            public const string InvalidMinimum = "Redemption_InvalidMinimum";
+            public const string Failed = "Redemption_Failed";
+            public const string LimitReached = "Redemption_LimitReached";
+            public const string BirthdayItemRedeemed = "Redemption_BirthdayItemRedeemed";
+            public const string InsufficientPoints = "Redemption_InsufficientPoints";
+            public const string SuccessfulRedemption = "Redemption_SuccessfulRedemption";
+            public const string SuccessfulSubmission = "Redemption_SuccessfulSubmission";
+            public const string VipOnly = "Redemption_VipOnly";
+            public const string Exception = "Redemption_Exception";
+            public const string Category = "Redemption_Category";
+            public const string Product = "Redemption_Product";
+            public const string Currency = "Redemption_Currency";
+            public const string Delivery = "Redemption_Delivery";
+            public const string Quantity = "Redemption_Quantity";
+            public const string Account = "Redemption_Account";
+            public const string EnterAccount = "Redemption_EnterAccount";
+            public const string EnterName = "Redemption_EnterName";
+            public const string EnterAddress = "Redemption_EnterAddress";
+            public const string EnterPostal = "Redemption_EnterPostal";
+            public const string EnterCity = "Redemption_EnterCity";
+            public const string EnterCountry = "Redemption_EnterCountry";
+            public const string EnterContactNumber = "Redemption_EnterContactNumber";
+            public const string RedeemNow = "Redemption_RedeemNow";
+            public const string RedeemInfo = "Redemption_RedeemInfo";
+            public const string Recipient = "Redemption_Recipient";
+            public const string EmailSendFail = "Redemption_EmailSendFail";
+        }
+
+        public static class Member
+        {
+            public const string Points = "Member_Points";
+            public const string PointLevel = "Member_PointLevel";
+        }
+    }
+}
