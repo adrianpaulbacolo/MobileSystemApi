07c226b99ea18f42be782cc48c33eda41242bb95
ajftolentino <josephtolentino@ymail.com>
2016-11-03 13:35:40 +0800

    Add RewardsController and SearchInfo classes

---
+++ W88.API/Controllers/RewardsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using W88.BusinessLogic.Rewards.Helpers;
+using W88.BusinessLogic.Rewards.Models;
+using W88.BusinessLogic.Shared.Models;
+
+namespace W88.API.Controllers
+{
+    [RoutePrefix("rewards")]
+    public class RewardsController : BaseController
+    {
+        [HttpPost]
+        [Route("search")]
+        public async Task<HttpResponseMessage> Search(SearchInfo searchInfo)
+        {
+            try
+            {
+                var response = await new RewardsHelper().SearchProducts(searchInfo);
+
+                return ReturnResponse(response);
+            }
+            catch (Exception ex)
+            {
+                return ReturnResponse(new ProcessCode
+                {
+                    Code = (int)Constants.StatusCode.Error,
+                    Message = ex.Message
+                }, ex);
+            }
+        }
+    }
+}
---
+++ W88.BusinessLogic/Rewards/Models/SearchInfo.cs
@@ -0,0 +1,23 @@
+﻿using W88.BusinessLogic.Accounts.Models;
+
+namespace W88.BusinessLogic.Rewards.Models
+{
+    public class SearchInfo
+    {
+        public string CategoryId = string.Empty;
+
+        public string Index = string.Empty;
+
+        public string PageSize = string.Empty;
+
+        public string SearchText = string.Empty;
+
+        public string SortBy = string.Empty;
+
+        public int MinPoints = 0;
+
+        public int MaxPoints = 0;
+
+        public MemberSession MemberSession = null;
+    }
+}
