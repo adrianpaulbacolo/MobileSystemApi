c36cc9f6f6985ba20731814bab73102036f18eeb
ajftolentino <josephtolentino@ymail.com>
2016-11-04 16:21:57 +0800

    Add [DisplayFormat(ConvertEmptyStringToNull = false)]
    - Add [DisplayFormat(ConvertEmptyStringToNull = false)] attribute in SearchInfo to string properties. This forces null values to be empty strings.

--- W88.BusinessLogic/Rewards/Models/SearchInfo.cs
+++ W88.BusinessLogic/Rewards/Models/SearchInfo.cs
@@ -1,48 +1,24 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+
 namespace W88.BusinessLogic.Rewards.Models
 {
     public class SearchInfo
     {
-        private string _categoryId;
+        [DisplayFormat(ConvertEmptyStringToNull = false)]
+        public string CategoryId { get; set; }
 
-        private string _index;
+        [DisplayFormat(ConvertEmptyStringToNull = false)]
+        public string Index { get; set; }
 
-        private string _pageSize;
+        [DisplayFormat(ConvertEmptyStringToNull = false)]
+        public string PageSize { get; set; }
 
-        private string _searchText;
+        [DisplayFormat(ConvertEmptyStringToNull = false)]
+        public string SearchText { get; set; }
 
-        private string _sortBy;
-
-        public string CategoryId
-        {
-            get { return _categoryId; }
-            set { _categoryId = string.IsNullOrEmpty(value) ? string.Empty : value; }
-        }
-
-        public string Index
-        {
-            get { return _index; }
-            set { _index = string.IsNullOrEmpty(value) ? string.Empty : value; }
-        }
-
-        public string PageSize
-        {
-            get { return _pageSize; }
-            set { _pageSize = string.IsNullOrEmpty(value) ? string.Empty : value; }
-        }
-
-        public string SearchText
-        {
-            get { return _searchText; } 
-            set { _searchText = string.IsNullOrEmpty(value) ? string.Empty : value; }
-        }
-
-        public string SortBy
-        {
-            get { return _sortBy; }
-            set { _sortBy = string.IsNullOrEmpty(value) ? string.Empty : value; }
-        }
-
+        [DisplayFormat(ConvertEmptyStringToNull = false)]
+        public string SortBy { get; set; }
+        
         public int MinPoints { get; set; }
 
         public int MaxPoints { get; set; }
