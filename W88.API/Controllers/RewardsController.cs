394625c45cb61ebb640fceb9fb2059cc2a1be909
ajftolentino <josephtolentino@ymail.com>
2016-11-04 15:02:42 +0800

    Add FromUri attribute to argument
    - Add FromUri attribute to SearchInfo argument to ensure that parameters being passed in URI will be used to create an instance of a SearchInfo class.
    - Refactor RewardsHelper.cs. Use Convert.ToString method to convert objects that need to be converted to string.

--- W88.API/Controllers/RewardsController.cs
+++ W88.API/Controllers/RewardsController.cs
@@ -1,32 +1,31 @@
 ﻿using System;
-using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using W88.BusinessLogic.Rewards.Helpers;
 using W88.BusinessLogic.Rewards.Models;
 using W88.BusinessLogic.Shared.Models;
-using W88.Utilities;
 
-namespace W88.API.Controllers
+namespace W88.API
 {
     [RoutePrefix("rewards")]
     public class RewardsController : BaseController
     {
-        [HttpGet]
+        // GET api/<controller>
+        /// <summary>
+        ///     Used to search catalogue products
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="searchInfo">SearchInfo object that contains search parameters</param>
+        /// <returns>HttpResponseMessage</returns>
         [Route("search")]
-        public async Task<HttpResponseMessage> Search(HttpRequestMessage request)
+        [HttpGet]
+        public async Task<HttpResponseMessage> Search(HttpRequestMessage request, [FromUri] SearchInfo searchInfo)
         {
             try
             {
-                await CheckToken(request);
-                var parameters = request.GetQueryNameValuePairs().ToDictionary(x => x.Key, x => x.Value);
-                if (parameters.Count == 0) return ReturnResponse(UserRequest.Process);
-                if(string.IsNullOrEmpty(parameters["searchInfo"])) return ReturnResponse(UserRequest.Process);
-
-                var searchInfo = Common.DeserializeObject<SearchInfo>(parameters["searchInfo"]);           
+                await CheckToken(request);           
                 var response = await new RewardsHelper().SearchProducts(searchInfo, UserRequest.UserInfo);
-
                 return ReturnResponse(response);
             }
             catch (Exception ex)
--- W88.BusinessLogic/Rewards/Models/SearchInfo.cs
+++ W88.BusinessLogic/Rewards/Models/SearchInfo.cs
@@ -1,21 +1,50 @@
-﻿using W88.BusinessLogic.Accounts.Models;
-
+﻿
 namespace W88.BusinessLogic.Rewards.Models
 {
     public class SearchInfo
     {
-        public string CategoryId = string.Empty;
+        private string _categoryId;
 
-        public string Index = string.Empty;
+        private string _index;
 
-        public string PageSize = string.Empty;
+        private string _pageSize;
 
-        public string SearchText = string.Empty;
+        private string _searchText;
 
-        public string SortBy = string.Empty;
+        private string _sortBy;
 
-        public int MinPoints = 0;
+        public string CategoryId
+        {
+            get { return _categoryId; }
+            set { _categoryId = string.IsNullOrEmpty(value) ? string.Empty : value; }
+        }
 
-        public int MaxPoints = 0;
+        public string Index
+        {
+            get { return _index; }
+            set { _index = string.IsNullOrEmpty(value) ? string.Empty : value; }
+        }
+
+        public string PageSize
+        {
+            get { return _pageSize; }
+            set { _pageSize = string.IsNullOrEmpty(value) ? string.Empty : value; }
+        }
+
+        public string SearchText
+        {
+            get { return _searchText; } 
+            set { _searchText = string.IsNullOrEmpty(value) ? string.Empty : value; }
+        }
+
+        public string SortBy
+        {
+            get { return _sortBy; }
+            set { _sortBy = string.IsNullOrEmpty(value) ? string.Empty : value; }
+        }
+
+        public int MinPoints { get; set; }
+
+        public int MaxPoints { get; set; }
     }
 }
