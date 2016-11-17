a89955d55e8f2bec2d9f958217bc44e1aa0a4974
Bryan Basmayor <bryanuybasmayor@gmail.com>
2016-11-17 16:21:50 +0800

    static page for rebates

---
+++ W88.m/Rebates.aspx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Rebates : BasePage
+{
+    protected System.Xml.Linq.XElement xeResources = null;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+}
