ef4e28f4ba1a076b533aa235cf8fea2e89986b61
ajftolentino <josephtolentino@ymail.com>
2016-11-22 10:33:56 +0800

    Rewards mobile revamp
    - Use W88.BusinessLogic library for all backend transactions.
    - Apply new layout to all pages.
    - Delete unused and unnecessary files that include xmls, images and classes.

--- W88.Utilities/Geo/ServerHelpers.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Web;
-
-namespace W88.Utilities.Geo
-{
-    public class ServerHelpers
-    {
-        public string SiteUrl { get { return HttpContext.Current.Request.ServerVariables["SERVER_NAME"]; } }
-    }
-}
