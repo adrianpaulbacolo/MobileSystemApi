680e9a81e5d0c195b9b83a21f1b14ef9dd2346c4
jackohdo <jack.ohdo@mnltechnology.com>
2016-12-05 18:53:06 +0800

    Added detailed logging

--- W88.m/_Secure/AutoLogin.aspx.cs
+++ W88.m/_Secure/AutoLogin.aspx.cs
@@ -15,6 +15,10 @@ public partial class _Secure_AutoLogin : BasePage
             {
                 using (var client = new memberWSSoapClient())
                 {
+                    commonAuditTrail.appendLog(_username, "_Secure_AutoLogin", "Params", string.Empty, string.Empty,
+                        string.Empty, "-99", string.Empty, Request.Url.ToString(),
+                        string.Empty, string.Empty, true);
+
                     commonCookie.CookieIsApp = "1";
                     var palazzoPrefix = ConfigurationManager.AppSettings.Get("palazzo_account_prefix");
                     var rawUsername = Request.QueryString["username"];
@@ -23,17 +27,21 @@ public partial class _Secure_AutoLogin : BasePage
                         ? Request.QueryString["username"].Remove(0, palazzoPrefix.Length)
                         : Request.QueryString["username"];
 
-                    commonAuditTrail.appendLog(_username, "_Secure_AutoLogin", "Params", string.Empty, string.Empty, string.Empty, "-99", string.Empty, string.Empty, string.Empty, string.Empty, true);
+                    commonAuditTrail.appendLog(_username, "_Secure_AutoLogin", "Params", string.Empty, string.Empty,
+                        string.Empty, "-99", string.Empty, string.Format("{0}|{1}|{2}", rawUsername, _username, token),
+                        string.Empty, string.Empty, true);
 
                     var dsData = client.MemberAutoSigninFromApp(Convert.ToInt64(commonVariables.OperatorId), _username,
                         rawUsername, token,
                         Request.Url.ToString(), Request.UserHostAddress, Session.SessionID);
 
+                    commonAuditTrail.appendLog(_username, "_Secure_AutoLogin", "Response", string.Empty, string.Empty, string.Empty, "-99", string.Empty, dsData.Tables.Count.ToString(), string.Empty, string.Empty, true);
+
                     if (dsData.Tables.Count > 0 && dsData.Tables[0].Rows.Count > 0)
                     {
                         
                         var result = int.Parse(dsData.Tables[0].Rows[0]["RETURN_VALUE"].ToString());
-                        commonAuditTrail.appendLog(_username, "_Secure_AutoLogin", "Response", string.Empty, string.Empty, string.Empty, "-99", string.Empty, result.ToString(), string.Empty, string.Empty, true);
+                        commonAuditTrail.appendLog(_username, "_Secure_AutoLogin", "ReturnValue", string.Empty, string.Empty, string.Empty, "-99", string.Empty, result.ToString(), string.Empty, string.Empty, true);
 
                         if (result == 1)
                         {
