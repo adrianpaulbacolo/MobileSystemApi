8366e7477ee72f822ff1caac3901e17189b09dc6
jackohdo <jack.ohdo@mnltechnology.com>
2016-12-05 13:48:45 +0800

    Added logging

--- W88.m/_Secure/AutoLogin.aspx.cs
+++ W88.m/_Secure/AutoLogin.aspx.cs
@@ -23,13 +23,18 @@ public partial class _Secure_AutoLogin : BasePage
                         ? Request.QueryString["username"].Remove(0, palazzoPrefix.Length)
                         : Request.QueryString["username"];
 
+                    commonAuditTrail.appendLog(_username, "_Secure_AutoLogin", "Params", string.Empty, string.Empty, string.Empty, "-99", string.Empty, string.Empty, string.Empty, string.Empty, true);
+
                     var dsData = client.MemberAutoSigninFromApp(Convert.ToInt64(commonVariables.OperatorId), _username,
                         rawUsername, token,
                         Request.Url.ToString(), Request.UserHostAddress, Session.SessionID);
 
                     if (dsData.Tables.Count > 0 && dsData.Tables[0].Rows.Count > 0)
                     {
+                        
                         var result = int.Parse(dsData.Tables[0].Rows[0]["RETURN_VALUE"].ToString());
+                        commonAuditTrail.appendLog(_username, "_Secure_AutoLogin", "Response", string.Empty, string.Empty, string.Empty, "-99", string.Empty, result.ToString(), string.Empty, string.Empty, true);
+
                         if (result == 1)
                         {
                             new Members().SetSessions(dsData.Tables[0], null);
