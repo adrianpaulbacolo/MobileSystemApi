f999ee061d42936ce1e4ada0f98db7bc567d276a
Mark <mark.marquez@mnltechnology.com>
2017-01-31 12:40:31 +0800

    Funds: Move use api and masterpage to nextpay

---
+++ W88.m/_Static/JS/modules/gateways/nextpay.js
@@ -0,0 +1,35 @@
+﻿function GNextPay() {
+
+    var gatewayId = "";
+
+    var nextpay = {
+        init: init
+    };
+
+    return nextpay;
+
+    function init(gateway) {
+        gatewayId = gateway;
+        getBanks();
+    }
+
+    function getBanks() {
+        var url = w88Mobile.APIUrl + "/banks/vendor/" + gatewayId ;
+        $.ajax({
+            type: "GET",
+            url: url,
+            success: function (d) {
+                banks = d.ResponseData;
+
+                $('select[id$="drpBanks"]').append($('<option>').text(defaultSelect).attr('value', '-1'));
+                $('select[id$="drpBanks"]').val("-1").selectmenu("refresh");
+
+                _.forOwn(banks, function (data) {
+                    $('select[id$="drpBanks"]').append($('<option>').text(data.Text).attr('value', data.Value));
+                });
+            }
+        });
+    }
+}
+
+window.w88Mobile.Gateways.GNextPay = GNextPay();
