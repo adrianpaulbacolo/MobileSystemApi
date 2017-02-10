6f428475ea21a6bf3f6c360007d2d4898d2bf568
jackohdo <jack.ohdo@mnltechnology.com>
2017-02-10 10:29:13 +0800

    v2 revamp: Payment master update and alldebit

---
+++ W88.m/_Static/v2/assets/js/growl.js
@@ -0,0 +1,67 @@
+﻿function Notification() {
+    var Notification = {
+        options: {
+            hasButtons: false,
+            lang: 'en',
+            close: 'x',
+            ok: "OK"
+        },
+        init: function (msg, ok) {
+            var self = this;
+            if (self.modal == null) {
+                $('body').append(self.getTemplate(ok));
+                self.modal = $('#PopUpModal');
+            }
+            $("#ModalMessage").html(msg);
+            self.modal.popup({
+                afterclose: function (event, ui) { }
+            });
+        },
+        shout: function (msg, callback) {
+            this.init(msg, '');
+            this.modal.popup('open');
+
+            if (!_.isUndefined(callback) && _.isFunction(callback))
+                this.modal.on("popupafterclose", callback);
+        },
+        notif: function (msg) {
+            var self = this;
+
+            var okButton = '<div class="row row-no-padding"><div class="col">' +
+           '<a href="#" data-rel="back" class="ui-btn btn-primary">' + self.options.ok + '</a>' +
+           '</div>';
+
+            this.init(msg, okButton);
+            this.modal.popup('open');
+        },
+        getTemplate: function (ok) {
+            var self = this;
+
+            var template = '<div id="PopUpModal" data-role="popup" data-overlay-theme="b" data-theme="b" data-history="false">' +
+            '<a href="#" data-rel="back" class="close close-enhanced">&times;</a>' + '<br>' +
+            '<div class="padding">' +
+            '<div id="ModalMessage" class="download-app padding"></div>' +
+            '</div>' + ok + '</div></div>';
+
+            return template;
+        },
+        bulletedList: function (messages) {
+            var message = "";
+            if (_.isArray(messages)) {
+                message = "<ul>";
+
+                for (var i = 0; i < messages.length; i++) {
+                    message = message + "<li>" + messages[i] + "</li>";
+                }
+
+                return message + "</ul>";
+            }
+            return message;
+        },
+        modal: null
+    }
+
+    return Notification;
+}
+
+window.w88Mobile.Growl = Notification();
