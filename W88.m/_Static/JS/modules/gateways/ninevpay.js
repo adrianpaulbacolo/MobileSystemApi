7ce3db584fe03a2ab20db390d2a45a7c41c7230c
Ruby Sarah Sitchon <ruby.sitchon@mnltechnology.com>
2017-02-15 13:36:58 +0800

    Shengpay: implement new flow

--- W88.m/_Static/JS/modules/gateways/ninevpay.js
+++ W88.m/_Static/JS/modules/gateways/ninevpay.js
@@ -20,8 +20,6 @@ function NineVPay() {
                 case 1:
                     w88Mobile.PostPaymentForm.createv2(response.ResponseData.FormData, response.ResponseData.PostUrl, "body");
                     w88Mobile.PostPaymentForm.submit();
-
-                    $('#form1')[0].reset();
                     break;
                 default:
                     if (_.isArray(response.ResponseMessage))
