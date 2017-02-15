0fc1cc91a2f3c17ab4b63ffe7273f99728c7bb93
Ruby Sarah Sitchon <ruby.sitchon@mnltechnology.com>
2017-02-15 10:05:42 +0800

    Implement Paysec IDR deposit page

---
+++ W88.m/Deposit/PaySec2.aspx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web.UI;
+
+
+public partial class Deposit_PaySec2 : PaymentBasePage
+{
+    protected void Page_Init(object sender, EventArgs e)
+    {
+        base.PageName = Convert.ToString(commonVariables.DepositMethod.PaySec);
+        base.PaymentType = commonVariables.PaymentTransactionType.Deposit;
+        base.PaymentMethodId = Convert.ToString((int)commonVariables.DepositMethod.PaySec);
+    }
+}
