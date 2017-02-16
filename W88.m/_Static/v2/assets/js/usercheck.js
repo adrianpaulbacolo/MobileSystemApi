8d4c7d37362911d1235a25d555b12ccf2d1c0a66
Mark <mark.marquez@mnltechnology.com>
2017-02-16 16:28:12 +0800

    User: Add polling of user status
    - periodically checks user status
    - @todo should apply new modal based alerts

---
+++ W88.m/_Static/v2/assets/js/usercheck.js
@@ -0,0 +1,30 @@
+﻿$(window).load(function () {
+
+    if (typeof window.User != "undefined" && window.User.hasSession) checkSession();
+    var sessionPoll;
+
+    function checkSession() {
+        var intervalMin = 10000; // 10 secs
+        var sessionInterval = (typeof window.User != "undefined" && parseInt(window.User.sessionInterval) > intervalMin) ? parseInt(window.User.sessionInterval) : intervalMin;
+
+        sessionPoll = window.setInterval(function () {
+            $.ajax({
+                contentType: "application/json; charset=utf-8",
+                url: "/_secure/AjaxHandlers/MemberSessionCheck.ashx",
+                responseType: "json",
+                success: function (data) {
+                    if (data.code != "1") {
+                        //@todo use growl and add redirection in callback
+                        if (typeof data.message != "undefined") {
+                            alert(data.message);
+                        }
+                        window.location.replace("/Logout");
+                        clearInterval(sessionPoll);
+                    }
+                },
+                error: function (err) {
+                }
+            });
+        }, sessionInterval);
+    }
+});
