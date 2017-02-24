f63d46f1ac72f5320433770512b73d2cccf95e98
jackohdo <jack.ohdo@mnltechnology.com>
2017-02-24 18:37:34 +0800

    Fix bug

--- W88.m/v2/Deposit/Pay120218.aspx.cs
+++ W88.m/v2/Deposit/Pay120218.aspx.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class v2_Deposit_Pay120218 PaymentBasePage
+public partial class v2_Deposit_Pay120218 : PaymentBasePage
 {
     protected void Page_Init(object sender, EventArgs e)
     {
