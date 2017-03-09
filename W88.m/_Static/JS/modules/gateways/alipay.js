dc282daa4d15108d1c31740ea05db0d7002addc8
Ruby Sarah Sitchon <ruby.sitchon@mnltechnology.com>
2017-03-09 17:46:55 +0800

    Add switch line check box in auto-route page

--- W88.m/_Static/JS/modules/gateways/alipay.js
+++ W88.m/_Static/JS/modules/gateways/alipay.js
@@ -12,6 +12,7 @@
             if (_w88_contents.translate("LABEL_PAYMENT_NOTE") != "LABEL_PAYMENT_NOTE") {
                 $("#paymentNote").text(_w88_contents.translate("LABEL_PAYMENT_NOTE"));
                 $("#paymentNoteContent").text(_w88_contents.translate("LABEL_PAYMENT_NOTE1"));
+                $('label[id$="lblSwitchLine"]').text(_w88_contents.translate("LABEL_SWITCH_LINE"));
             } else {
                 window.setInterval(function () {
                     setTranslations();
