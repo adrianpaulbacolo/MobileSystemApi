4027c979ba25702ba14f5993e4796bfaadcb3679
Ruby Sarah Sitchon <ruby.sitchon@mnltechnology.com>
2017-04-04 11:52:22 +0800

    Fund Transfer: accept transferto param

--- W88.m/_Static/JS/Main.js
+++ W88.m/_Static/JS/Main.js
@@ -178,3 +178,7 @@ function TwoDecimalAndroid(ctrl, event) {
         event.preventDefault();
     }
 }
+
+function getQueryStringValue(key) {
+    return decodeURIComponent(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + encodeURIComponent(key).replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));
+}
