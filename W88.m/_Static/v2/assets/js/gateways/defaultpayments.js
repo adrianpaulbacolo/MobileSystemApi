eb2d8f3f5fe0e0e4d5199fced3a77c459567378d
Ruby Sarah Sitchon <ruby.sitchon@mnltechnology.com>
2017-04-21 17:56:48 +0800

    Fix history button layout

--- W88.m/_Static/v2/assets/js/gateways/defaultpayments.js
+++ W88.m/_Static/v2/assets/js/gateways/defaultpayments.js
@@ -205,7 +205,7 @@ function DefaultPaymentsV2() {
 
     function onTransactionCreated(form) {
 
-        var historyBtn = "<a href='/v2/History/Default.aspx' class='btn btn-primary' data-ajax='false'>" + _w88_contents.translate("LABEL_FUNDS_HISTORY") + "</a>";
+        var historyBtn = "<a href='/v2/History/Default.aspx' class='btn btn-block btn-primary' data-ajax='false'>" + _w88_contents.translate("LABEL_FUNDS_HISTORY") + "</a>";
         var message = "<p>" + _w88_contents.translate("MESSAGES_CHECK_HISTORY") + "</p>" + historyBtn;
         if (!_.isUndefined(form)) _.first(form).reset();
         w88Mobile.Growl.shout(message);
