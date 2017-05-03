14a03a0ab71e36ab8bcffb7d0cffe66c9547af6a
Mark <mark.marquez@mnltechnology.com>
2017-05-03 14:08:30 +0800

    Lottery: Add lottery page for v2

---
+++ W88.m/v2/Lottery.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class v2_Lottery : BasePage
+{
+    protected override void OnLoad(EventArgs e)
+    {
+        Page.Title = commonCulture.ElementValues.getResourceString("lottery", commonVariables.LeftMenuXML);
+        Page.Items.Add("Parent", "/v2/Dashboard.aspx");
+        base.OnLoad(e);
+    }
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+}
