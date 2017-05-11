a98758265740d597120c2e5f9cbf05fdf1fca05e
Ruby Sarah Sitchon <ruby.sitchon@mnltechnology.com>
2017-05-11 13:39:03 +0800

    Update change password parent

--- W88.m/v2/ChangePassword.aspx.cs
+++ W88.m/v2/ChangePassword.aspx.cs
@@ -4,7 +4,7 @@ public partial class v2_ChangePassword : BasePage
 {
     protected override void OnLoad(EventArgs e)
     {
-        Page.Items.Add("Parent", "/v2/Funds.aspx");
+        Page.Items.Add("Parent", "/v2/Dashboard.aspx");
         base.OnLoad(e);
     }
 }
