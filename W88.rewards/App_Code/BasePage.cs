09fec0a0f89754a7d804840d807ebb099b32f2a7
ajftolentino <josephtolentino@ymail.com>
2017-05-13 13:27:58 +0800

    Add Token property to BasePage class
    - To facilitate setting and retrieval of token from cookie, add Token property that gets and sets token from and to cookies, respectively.

--- W88.rewards/App_Code/BasePage.cs
+++ W88.rewards/App_Code/BasePage.cs
@@ -8,6 +8,7 @@ using W88.BusinessLogic.Rewards.Helpers;
 using W88.BusinessLogic.Rewards.Models;
 using W88.BusinessLogic.Shared.Helpers;
 using W88.Utilities;
+using W88.Utilities.Geo;
 
 public class BasePage : Page
 {
@@ -18,6 +19,7 @@ public class BasePage : Page
     protected Members MembersHelper = new Members();
     protected RewardsHelper RewardsHelper = new RewardsHelper();
     protected string Language = string.Empty;
+    protected static readonly IpHelper IpHelper = new IpHelper();
 
     protected bool IsUnderMaintenance
     {
@@ -82,6 +84,22 @@ public class BasePage : Page
         }
     }
 
+    public static string Token 
+    {
+        get 
+        { 
+            var cookie = HttpContext.Current.Request.Cookies.Get("token");
+            return cookie == null ? string.Empty : cookie.Value;
+        }
+        set
+        {
+            var cookie = HttpContext.Current.Request.Cookies.Get("token") ?? new HttpCookie("token");
+            cookie.Value = value;
+            cookie.Domain = IpHelper.DomainName;
+            HttpContext.Current.Response.Cookies.Add(cookie);
+        }
+    }
+
     protected override async void OnPreInit(EventArgs e)
     {
         base.OnPreInit(e);
@@ -121,9 +139,7 @@ public class BasePage : Page
                 token = HttpContext.Current.Request.Headers.Get("token");
                 if (string.IsNullOrEmpty(token))
                 {
-                    var cookie = Request.Cookies.Get("token");
-                    if (cookie == null) return false;
-                    token = cookie.Value;
+                    token = Token;
                 }
                 if (string.IsNullOrEmpty(token)) return false;
             }
