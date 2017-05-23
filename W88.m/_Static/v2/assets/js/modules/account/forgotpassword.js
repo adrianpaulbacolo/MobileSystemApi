c25be913f76efde0b18cd1514c84dd7e1caf9b81
jackohdo <jack.ohdo@mnltechnology.com>
2017-05-23 10:19:49 +0800

    v2 Forogot password

---
+++ W88.m/_Static/v2/assets/js/modules/account/forgotpassword.js
@@ -0,0 +1,157 @@
+﻿window.w88Mobile.ForgotPassword = ForgotPassword();
+var _w88_ForgotPassword = window.w88Mobile.ForgotPassword;
+
+function ForgotPassword() {
+
+    var forgot = {};
+    var key = "forgot_login";
+    var forgotData = {
+        Username: {},
+        Email: {},
+        QuestionId: 0,
+        Answer: '',
+        LastRequested: {},
+        isPartial: true
+    };
+
+    forgot.init = function () {
+
+        $('.header-title').first().text($.i18n("LABEL_PASSWORD_FORGOT"));
+
+        amplify.store(key, forgotData, window.User.storageExpiration);
+        var step1Template = _.template($("script#Step1Template").html());
+
+        $("#forgot").html(step1Template({
+            lblUsername: $.i18n("LABEL_USERNAME"),
+            lblEmail: $.i18n("LABEL_EMAIL"),
+            lblSubmit: $.i18n("BUTTON_SUBMIT")
+        }));
+
+        _w88_validator.initiateValidator($("#form1"), {});
+    };
+
+    forgot.fetchQuestions = function () {
+
+        _w88_ForgotPassword.send("", "/SecurityQuestions", "GET", function (response) {
+            if (_.isEqual(response.ResponseCode, 1)) {
+                _.forOwn(response.ResponseData, function (data) {
+                    $('#questions').append($('<option>').text(data.Text).attr('value', data.Value));
+                });
+                $("#questions").val($("#questions option:first").val()).change();
+                $('#form1').validator('update').validator();
+            }
+        });
+    };
+
+    forgot.checkPartial = function () {
+
+        var hasErrors = $('#form1').validator('validate').has('.has-error').length;
+
+        if (!hasErrors) {
+
+            forgotData.Username = $("#txtUsername").val();
+            forgotData.Email = $("#txtEmail").val();
+
+            amplify.store(key, forgotData, window.User.storageExpiration);
+
+            var d = {
+                username: forgotData.Username,
+                email: forgotData.Email
+            };
+
+            _w88_ForgotPassword.send(d, "/user/CheckPartialRegistration", "GET", function (response) {
+                if (_.isEqual(response.ResponseCode, 1)) {
+
+                    switch (response.ResponseData) {
+                        case 1:
+                            forgotData.isPartial = true;
+                            amplify.store(key, forgotData, window.User.storageExpiration);
+                            _w88_ForgotPassword.submit();
+                            break;
+
+                        case 2:
+                            forgotData.isPartial = false;
+                            amplify.store(key, forgotData, window.User.storageExpiration);
+
+                            var step2Template = _.template($("script#Step2Template").html());
+
+                            $("#forgot").html(step2Template({
+                                lblQuestion: $.i18n("LABEL_SECURITY_QUESTION"),
+                                lblAnswer: $.i18n("LABEL_SECURITY_ANSWER"),
+                                lblSubmit: $.i18n("BUTTON_SUBMIT")
+                            }));
+
+                            _w88_ForgotPassword.fetchQuestions();
+
+                            break;
+                        default:
+                            window.w88Mobile.Growl.shout(response.ResponseMessage);
+                            break;
+                    }
+                }
+            });
+        }
+    };
+
+    forgot.submit = function () {
+        forgotData = amplify.store(key);
+
+        if (forgotData.isPartial) {
+            forgotData.QuestionId = 0;
+            forgotData.Answer = '';
+        } else {
+
+            var hasErrors = $('#form1').validator('validate').has('.has-error').length;
+
+            if (!hasErrors) {
+                forgotData.QuestionId = $("#questions").val();
+                forgotData.Answer = $("#txtSecurityAnswer").val();
+            } else return false;
+        }
+
+        forgotData.LastRequested = Cookies().getCookie(key);
+
+        _w88_ForgotPassword.send(forgotData, "/user/ForgotPassword", "POST", function (response) {
+            if (_.isEqual(response.ResponseCode, 1)) {
+
+                Cookies().setCookie(key, response.ResponseData);
+                window.w88Mobile.Growl.shout(response.ResponseMessage, function () {
+                    window.location.reload();
+                });
+
+            } else {
+                window.w88Mobile.Growl.shout(response.ResponseMessage);
+            }
+        });
+    };
+
+    forgot.send = function (data, resource, method, success, complete) {
+
+        var url = w88Mobile.APIUrl + resource;
+
+        var headers = {
+            'Token': window.User.token,
+            'LanguageCode': window.User.lang
+        };
+
+        $.ajax({
+            type: method,
+            url: url,
+            data: data,
+            beforeSend: function () {
+                pubsub.publish('startLoadItem', { selector: "" });
+            },
+            headers: headers,
+            success: success,
+            error: function () {
+                console.log("Error connecting to api");
+            },
+            complete: function () {
+                if (!_.isUndefined(complete)) complete();
+                pubsub.publish('stopLoadItem', { selector: "" });
+            }
+        });
+    };
+
+    return forgot;
+}
---
+++ W88.m/v2/Account/ForgotPassword.aspx
@@ -0,0 +1,55 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/v2/MasterPages/Main.master" AutoEventWireup="true" CodeFile="ForgotPassword.aspx.cs" Inherits="v2_Account_ForgotPassword" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContentHolder" runat="Server">
+    <div class="form-container">
+        <div class="container">
+            <form class="form" id="form1" runat="server">
+                <div id="forgot"></div>
+
+            </form>
+        </div>
+    </div>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="head" runat="Server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="InnerScriptPlaceHolder" runat="Server">
+    <script src="<%=ConfigurationManager.AppSettings.Get("AssetsPath") %>/assets/js/modules/bootstrapvalidator.js?v=<%=ConfigurationManager.AppSettings.Get("scriptVersion") %>"></script>
+    <script src="<%=ConfigurationManager.AppSettings.Get("AssetsPath") %>/assets/js/modules/account/forgotpassword.js?v=<%=ConfigurationManager.AppSettings.Get("scriptVersion") %>"></script>
+
+    <script>
+        $(document).ready(function () {
+
+            _w88_ForgotPassword.init();
+        });
+    </script>
+
+    <script type="text/template" id='Step1Template'>
+        <div class="form-group">
+            <label>{%- tplData.lblUsername %}</label>
+            <input id="txtUsername" class="form-control" maxlength="16" required data-require="RequiredField" />
+        </div>
+        <div class="form-group ">
+            <label>{%- tplData.lblEmail %}</label>
+            <input id="txtEmail" class="form-control" required data-require="RequiredField" />
+        </div>
+        <div class="form-group">
+            <button type="submit" id="btnSubmit" class="btn btn-block btn-primary" onclick="javascript: _w88_ForgotPassword.checkPartial(); return false;">{%- tplData.lblSubmit %}</button>
+        </div>
+
+    </script>
+
+    <script type="text/template" id='Step2Template'>
+        <div class="form-group">
+            <label>{%- tplData.lblQuestion%}</label>
+            <select id="questions" class="form-control" required data-selectequals="0"></select>
+        </div>
+        <div class="form-group">
+            <label>{%- tplData.lblAnswer%}</label>
+            <input id="txtSecurityAnswer" class="form-control" required data-require="RequiredField" />
+        </div>
+        <div class="form-group">
+            <button type="submit" id="btnSendForgot" class="btn btn-block btn-primary" onclick="javascript: _w88_ForgotPassword.submit(); return false;">{%- tplData.lblSubmit %}</button>
+        </div>
+    </script>
+</asp:Content>
+
