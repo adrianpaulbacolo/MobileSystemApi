30c47a30165dbf376a711623d821e86b414a15be
Ruby Sarah Sitchon <ruby.sitchon@mnltechnology.com>
2017-05-24 11:01:23 +0800

    Add isPublic = false

--- W88.m/v2/Account/ChangePassword.aspx.cs
+++ W88.m/v2/Account/ChangePassword.aspx.cs
@@ -2,6 +2,11 @@
 
 public partial class v2_Account_ChangePassword : BasePage
 {
+    protected override void OnPreInit(EventArgs e)
+    {
+        this.isPublic = false;
+    }
+
     protected override void OnLoad(EventArgs e)
     {
         Page.Items.Add("Parent", "/v2/Dashboard.aspx");
