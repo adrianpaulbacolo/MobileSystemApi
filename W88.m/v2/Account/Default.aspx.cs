eb2059a4a8c955b14727f4efb6163c3ef1490793
jackohdo <jack.ohdo@mnltechnology.com>
2017-05-24 10:54:24 +0800

    use basepage

--- W88.m/v2/Account/Default.aspx.cs
+++ W88.m/v2/Account/Default.aspx.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using Models;
 
-public partial class v2_Account_Default : PaymentBasePage
+public partial class v2_Account_Default : BasePage
 {
     protected override void OnLoad(EventArgs e)
     {
         Page.Items.Add("Parent", W88Constant.PageNames.Dashboard);
         base.OnLoad(e);
+    }
+
+    protected override void OnPreInit(EventArgs e)
+    {
+        this.isPublic = false;
     }
 
     protected void Page_Load(object sender, EventArgs e)
